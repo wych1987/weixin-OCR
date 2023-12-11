c6d88f94899651560b4123f953c34023ee54195c
yunchenw <wangyunchen@163.com>
2023-12-11 20:57:00 +0800

    add ocr action btn

--- miniprogram/pages/ocr/index.js
+++ miniprogram/pages/ocr/index.js
@@ -1,16 +1,42 @@
 import { drawImage ,drawRectByOCR} from "../../utils/index"
 import {mockData} from "../../utils/mock"
 Page({
-
+//https://cloud.tencent.com/document/product/866/80801
   /**
    * 页面的初始数据
    */
   data: {
     showUploadTip: false,
     haveGetRecord: false,
-    envId: '',
-    record: '',
-    imageSrc:"https://noway-img.oss-rg-china-mainland.aliyuncs.com/fa2ec2875d501ab2d60862bec74427d1.JPG"
+    actionList:[{
+      text:"切边",// + 弯曲矫正
+      tasktype:[1,2]
+    },
+    {
+      text:"去噪",
+      tasktype:[301,302,303]//去除模糊,去摩尔纹
+    },
+    {
+      text:"高亮",
+      tasktype:[204]
+    },
+    {
+      text:"灰度",
+      tasktype:[205]
+    },
+    {
+      text:"黑白",
+      tasktype:[202]
+    },
+    {
+      text:"省墨",
+      tasktype:[207]
+    },
+    {
+      text:"锐化",
+      tasktype:[208]
+    }
+  ]
   },
 
   onLoad(options) {
@@ -22,14 +48,18 @@ Page({
       .fields({ node: true, size: true })
       .exec(async(res) => {
       //  const dpr = wx.getSystemInfoSync().pixelRatio
-      const canvas = res[0].node
-      const ctx = canvas.getContext('2d');
+        const canvas = res[0].node
+        const ctx = canvas.getContext('2d');
      
-        const img = 'https://noway-img.oss-rg-china-mainland.aliyuncs.com/fa2ec2875d501ab2d60862bec74427d1.JPG'
-        drawImage(canvas, img).then(res => {
+        // const img = 'https://noway-img.oss-rg-china-mainland.aliyuncs.com/fa2ec2875d501ab2d60862bec74427d1.JPG'
+        const imgTX = "https://img-1320809449.cos.ap-shanghai.myqcloud.com/fa2ec2875d501ab2d60862bec74427d1.JPG"
+        drawImage(canvas, imgTX).then(res => {
            drawRectByOCR(ctx,mockData.TextDetections)
         })
-        
       })
   },
+  actionClick(e){
+    const tasktype = e.currentTarget.dataset.tasktype;
+    console.log("===TaskType===",tasktype);
+  }
 });
--- miniprogram/utils/index.js
+++ miniprogram/utils/index.js
@@ -1,35 +1,34 @@
 export function drawImage(canvas, imgSrc) {
+   const ctx = canvas.getContext('2d')
   return new Promise((reslove, reject) => {
     const img = canvas.createImage()
-
     img.onload = () => {
-      canvas.height = img.naturalHeight;
-      canvas.width = img.naturalWidth;
-      const ctx = canvas.getContext('2d')
-      ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight);
-      reslove({success:true,data:{}})
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0, img.width, img.height);
+      reslove({ success: true, data: {} })
     };
-    img.onerror = () => {
+    img.onerror = (error) => {
+      console.error("=====error===",error)
       reject({ success: false, error: "imgloadError" })
     }
     img.src = imgSrc;
   })
 }
 
-export function drawRectByOCR (ctx,ocrData){
-    ctx.strokeStyle = "#cf157d94";
-    ctx.fillStyle = "#ff890756";
-    ocrData.forEach(item=>{
-      drawByPath(ctx,item.Polygon)
-    })
+export function drawRectByOCR(ctx, ocrData) {
+  ctx.strokeStyle = "#cf157d94";
+  ctx.fillStyle = "#ff890756";
+  ocrData.forEach(item => {
+    drawByPath(ctx, item.Polygon)
+  })
 }
-function drawByPath(ctx,pathArray){
-
+function drawByPath(ctx, pathArray) {
   ctx.beginPath();
-  const {X,Y} = pathArray[0];
+  const { X, Y } = pathArray[0];
   ctx.moveTo(X, Y);
-  for(let index = 1;index<pathArray.length;index++){
-    const {X,Y} = pathArray[index];
+  for (let index = 1; index < pathArray.length; index++) {
+    const { X, Y } = pathArray[index];
     ctx.lineTo(X, Y);
   }
   ctx.fill();
