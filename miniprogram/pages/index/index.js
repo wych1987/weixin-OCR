4174369fe4f8a00bc8071ca50067fae45d0a3817
yunchenw <wangyunchen@163.com>
2023-12-15 21:42:44 +0800

    add image OCR handlewrite

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -6,28 +6,33 @@ Page({
       tip: '快捷方便去掉手写痕迹',
       showItem: false,
       page: 'picProcessing'
-    } ,
+    },
     {
       title: '口算训练',
       tip: '两位数的口算训练',
       showItem: false,
-      page:"numberTopic"
+      page: "numberTopic"
     },
     {
       title: '手动擦除手写体',
       tip: '---还未开发---',
       showItem: false,
     }
-  ]
+    ]
   },
   jumpPage(e) {
     const page = e.currentTarget.dataset.page;
-    if(!page) return false;
+    if (!page) {
+      wx.showToast({
+        title: '敬请期待',
+      })
+      return false;
+    }
     wx.navigateTo({
       url: `/pages/${e.currentTarget.dataset.page}/index`,
     });
   },
 
-   
- 
+
+
 });
