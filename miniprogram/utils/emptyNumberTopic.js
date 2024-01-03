b0f98ccc4d923aa3e364af0da5b096be08fa2dff
yunchenw <wangyunchen@163.com>
2024-01-03 17:31:48 +0800

    Add u

--- miniprogram/utils/emptyNumberTopic.js
+++ miniprogram/utils/emptyNumberTopic.js
@@ -12,13 +12,21 @@ export const emptyTopic = (level)=>{
         num2:{
             number: res.num2,
             strArray:String(res.num2).split("")
-        }
+        },
+        result:{
+            number:res.result,
+            strArray:res.resultStrArray,
+            
+        },
+        symbolStr:res.symbolStr,
+        symbolNum:res.symbolNum
     }
   
     switch(level){
       case 1:
       // 1则挖两个坑  
       // 
+      empty2(topic)
       break;
       case 2:
        // 2则挖3个坑    
@@ -36,9 +44,29 @@ export const emptyTopic = (level)=>{
     const random = Math.random()>0.5;
     if(random){
         // 在num1上挖坑
-
+        topic.num1.show = transferTopic(topic.strArray,1)
     }else{
         // 在num2 上挖坑
+        topic.num1.show = transferTopic(topic.strArray,0)
     }
-     
+     // 在answer上挖坑
+     topic.result.show = transferTopic(topic.strArray,topic.result.number.length-1)
+  }
+
+  function transferTopic(numberArray,index){
+    const res = [];
+    for(let i = 0; i <numberArray.length;i++){
+        if(index===i){
+            res.push({
+                value:numberArray[i],
+                type:"input"
+            })
+        }else{
+            res.push({
+                value:numberArray[i],
+                type:"text"
+            })
+        }
+    }
+    return res;
   }
